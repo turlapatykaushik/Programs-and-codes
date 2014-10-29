42df5b5579173056b721f8f65a54ee3cf985651c
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-10-30 00:59:18 +0530

    Update prime_number_generator.py

--- prime_number_generator.py
+++ prime_number_generator.py
@@ -2,4 +2,9 @@
 * @turlapatykaushik
 * github url : github.com/turlapatykaushik
 
-problem description : program to generate all the prime numbers in a given range
+* problem description : program to generate all the prime numbers in a given range
+*/
+
+for num in range(2,100000):
+    if all(num%i!=0 for i in range(2,num)):
+       print num
