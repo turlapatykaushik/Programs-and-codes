bf29e5db6609f2c453afd1d94085e9602bcd82cc
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-10-30 00:51:52 +0530

    Update life_universe_everything.py

--- life_universe_everything.py
+++ life_universe_everything.py
@@ -1,6 +1,8 @@
 /*
 * @turlapatykaushik
 * github url : github.com/turlapatykaushik
+
+* problem description : solution to Life universe and everything problem in spoj and codechef in Python
 */
 while(1):
   i = int(raw_input())
