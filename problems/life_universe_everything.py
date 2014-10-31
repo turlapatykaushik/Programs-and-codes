7404561821ff434ea3716d43e7040c8bdd754aa7
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-10-31 15:23:33 +0530

    Create life_universe_everything.py

---
+++ problems/life_universe_everything.py
@@ -0,0 +1,12 @@
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+
+* problem description : solution to Life universe and everything problem in spoj and codechef in Python
+*/
+while(1):
+  i = int(raw_input())
+  if(i == 42):
+    break
+  else:
+    print i
