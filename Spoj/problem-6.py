c11aa65f9575124a85a4ca6e1252a21457ec14ec
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-03 17:48:10 +0530

    Create problem-6.py

---
+++ Spoj/problem-6.py
@@ -0,0 +1,13 @@
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+ */
+ 
+ summ = 0
+for i in range(1,101):
+  k = i * i
+  summ = summ + k
+print summ
+
+print 25502500 - summ
+  
