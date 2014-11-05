85525f0b38c1c41dd2d1d2a1a6ab7a1678bb4d14
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-06 04:15:27 +0530

    Create intro_tut_challenges.py

---
+++ HackerRank/intro_tut_challenges.py
@@ -0,0 +1,10 @@
+/*
+ * @turlapatykaushik
+ * github url : github.com/turlapatykaushik
+ * problem description : intro to tutorial challenges
+*/
+
+k = input()
+p = input()
+a = [int(x) for x in raw_input().split()]
+print a.index(k)
