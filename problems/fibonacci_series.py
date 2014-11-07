7d6c9115b9f136c33258038fbc547e443c9f56f9
Turlapaty Kaushik <turlapatykaushik95@gmail.com>
2014-11-07 22:27:23 +0530

    Added

---
+++ problems/fibonacci_series.py
@@ -0,0 +1,11 @@
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+
+* problem description : Find the sequence of the fibonacci series for some given
+  n. 
+*/
+a , n = 0,1
+while n < 1000:
+    print n
+    a,n = n,a+n
