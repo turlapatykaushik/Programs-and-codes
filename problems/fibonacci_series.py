afe6931bd818a66fb6087ae3c4ce6318d8dff845
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-08 03:59:54 +0530

    Update fibonacci_series.py

--- problems/fibonacci_series.py
+++ problems/fibonacci_series.py
@@ -2,9 +2,9 @@
 * @turlapatykaushik
 * github url : github.com/turlapatykaushik
 
-* problem description : Find the sequence of the fibonacci series for some given
-  n. 
+* problem description : Find the sequence of the fibonacci series for some given n
 */
+
 a , n = 0,1
 while n < 1000:
     print n
