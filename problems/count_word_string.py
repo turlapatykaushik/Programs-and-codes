23323e21fafee4ee627fb4cb44f3243b0467f179
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-09 16:25:09 +0530

    Create count_word_string.py

---
+++ problems/count_word_string.py
@@ -0,0 +1,11 @@
+/*
+ * @turlapatykaushik
+ * github url : github.com/turlapatykaushik
+
+ * problem description : Count the number of words in a string
+*/
+
+x = raw_input("Enter the string to count the words ")
+y = x.split()
+print len(y)
+
