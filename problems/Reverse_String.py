32a6bccf5bf90bccc33b9e5f7f146c962d1473f9
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2014-11-09 15:52:21 +0530

    Create Reverse_String.py

---
+++ problems/Reverse_String.py
@@ -0,0 +1,9 @@
+/*
+* @turlapatykaushik
+* github url : github.com/turlapatykaushik
+
+* problem description : Python program to reverse a string
+*/
+x = raw_input("Enter a string to reverse ")
+y = x[::-1]
+print y
