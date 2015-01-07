5518c594fec62ea3c67451b4ec57709a799aeaa6
turlapatykaushik <kaushik.turlapaty@iiitb.org>
2015-01-07 19:54:31 +0530

    added

---
+++ Project-Euler/smallest_multiple_problem5.py
@@ -0,0 +1,7 @@
+from fractions import gcd
+from functools import reduce
+def lcm(a,b):
+    "Calculate the lowest common multiple of two integers a and b"
+    return a*b//gcd(a,b)
+k = reduce(lcm, range(1,20+1))
+print k
