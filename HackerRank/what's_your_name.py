770ca5be4fb794cc5743b0e242c0df9e7afdc7d3
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-01-28 11:46:44 +0530

    Create what's_your_name.py

---
+++ HackerRank/what's_your_name.py
@@ -0,0 +1,10 @@
+/*
+ * @turlapatykaushik
+ * github url : github.com/turlapatykaushik
+ * problem description : Basic Python task
+*/
+
+FirstName = raw_input()
+secondName = raw_input()
+
+print "Hello " + FirstName + " " + secondName + "!" + " You just delved into python." 
