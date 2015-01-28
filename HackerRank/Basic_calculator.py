7c9114b6c983a0c8582061dc64223aa673e29e51
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-01-28 11:47:58 +0530

    Create Basic_calculator.py

---
+++ HackerRank/Basic_calculator.py
@@ -0,0 +1,20 @@
+/*
+ * @turlapatykaushik
+ * github url : github.com/turlapatykaushik
+ * problem description : Basic calculator in Python
+*/
+
+x = float(input())
+y = float(input())
+
+p = x+y
+o = x-y
+r = x*y
+s = x/y
+t = x//y
+
+print "%.2f" %p
+print "%.2f" %o
+print "%.2f" %r
+print "%.2f" %s
+print "%.2f" %t
