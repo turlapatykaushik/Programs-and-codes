d64bb69554b8ef7585e7cc74f02ba60438a311a0
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-02-01 12:58:28 +0530

    Update small_Factorials.py

--- HackerEarth/small_Factorials.py
+++ HackerEarth/small_Factorials.py
@@ -1,12 +1,10 @@
 def fac(n):
     if(n==0):
-		
-		return 1
+	return 1
     elif(n==1):
-		return 1
+	return 1
     else:
-		return n * fac(n-1)
-     
+	return n * fac(n-1)
 t = input()
 while(t):
 	t = t-1
