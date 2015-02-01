f2e4850c08b2671889766a53b726b162aa6ce955
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-02-01 13:38:08 +0530

    Update roy_and_profile_picture.py

--- HackerEarth/roy_and_profile_picture.py
+++ HackerEarth/roy_and_profile_picture.py
@@ -3,3 +3,16 @@
 * github url : github.com/turlapatykaushik
 * problem description : This program is 'roy and profile picture from HackerEarth
 */
+
+    l = input()
+    t = input()
+    while(t):
+    t = t-1
+    w,h = map(int, raw_input().split())
+    if( w < l)or ( h < l):
+    print "UPLOAD ANOTHER"
+    else:
+    if(w==h):
+    print "ACCEPTED"
+    else:
+    print "CROP IT"
