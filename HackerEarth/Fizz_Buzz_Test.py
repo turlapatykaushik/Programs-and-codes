09df6290b2a249654d7cef65d2407d019e0de3f2
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-02-01 13:48:05 +0530

    Update Fizz_Buzz_Test.py

--- HackerEarth/Fizz_Buzz_Test.py
+++ HackerEarth/Fizz_Buzz_Test.py
@@ -3,3 +3,13 @@
 * github url : github.com/turlapatykaushik
 * problem description : This program is about the problem 'Fizz Buzz Test' from HackerEarth
 */
+
+for i in range(1,101):
+  if(i%3==0)and(i%5!=0):
+    print "Fizz"
+  elif(i%5==0)and(i%3!=0):
+    print "Buzz"
+  elif(i%3==0)and(i%5==0):
+    print "FizzBuzz"
+  else:
+    print i
