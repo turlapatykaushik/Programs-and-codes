b9cc200452429c6c7043d9835aeeec0d811a2f2f
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-02-02 00:00:34 +0530

    Update what_is_the_string_made_of.py

--- HackerEarth/what_is_the_string_made_of.py
+++ HackerEarth/what_is_the_string_made_of.py
@@ -4,3 +4,10 @@
 * problem description : This program is about the problem 'what is the string made of' from HackerEarth
 */
 
+dict = { '1' : 2, '2' : 5, '3' : 5, '4' :4, '5' : 5, '6' : 6, '7' : 3, '8' : 7, '9': 6, '0':6}
+x = list(raw_input())
+sum = 0
+for i in range(len(x)):
+	sum = sum + dict[x[i]]
+print sum
+
