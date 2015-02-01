7638d422dc19c32d75cf4aa5452a0e6ad5ad0500
Turlapaty Kaushik <kaushik.turlapaty@iiitb.org>
2015-02-01 13:39:11 +0530

    Update roy_and_profile_picture.py

--- HackerEarth/roy_and_profile_picture.py
+++ HackerEarth/roy_and_profile_picture.py
@@ -4,15 +4,15 @@
 * problem description : This program is 'roy and profile picture from HackerEarth
 */
 
-    l = input()
-    t = input()
-    while(t):
+l = input()
+t = input()
+while(t):
     t = t-1
     w,h = map(int, raw_input().split())
     if( w < l)or ( h < l):
-    print "UPLOAD ANOTHER"
+        print "UPLOAD ANOTHER"
     else:
-    if(w==h):
-    print "ACCEPTED"
-    else:
-    print "CROP IT"
+        if(w==h):
+            print "ACCEPTED"
+        else:
+            print "CROP IT"
