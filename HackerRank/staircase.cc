b4c78060177c6e96f63748af2cfc433a11f567bd
turlapatykaushik <turlapatykaushik95@gmail.com>
2015-08-01 01:50:33 +0530

    Staircase Added

---
+++ HackerRank/staircase.cc
@@ -0,0 +1,20 @@
+   #include<iostream>
+   using namespace std;
+   int main()
+        {
+                int i,j,n;
+                cin>>n;
+                for(int i=n;i>=1;i--)
+                {
+                   for(int j=1;j<=n;j++)
+                   {
+                     if(j>=i)
+                   cout<<"#";
+                    else
+                    cout<<" ";
+
+                   }
+                    cout<<"\n";
+                }
+                return 0;
+        }
