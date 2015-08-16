b8b6f9e7719705808c882f85be402b57c2e011f1
turlapaty kaushik <kaushik.turlapaty@iiitb.org>
2015-08-17 03:22:33 +0530

    stringManipulator.cc Added

---
+++ problems/stringManipulator.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+string s = "Kaushik";
+int i = s.size();
+cout << i << endl;
+char c = s[2];
+s += "Turlapaty";
+cout << s << endl;
+return 0;
+}
