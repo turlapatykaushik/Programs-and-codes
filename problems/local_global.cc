94dcf87db7ee4b35c317b26b8b4a4250eab2563d
turlapatykaushik <turlapatykaushik95@gmail.com>
2015-08-19 12:01:45 +0530

    Added

---
+++ problems/local_global.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+const int x = 4;
+int y  = x;
+
+int main()
+{
+    const int x = 5;
+    cout << x<< endl;
+    cout << y<< endl;
+    return 0;
+}
+
